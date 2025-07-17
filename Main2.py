0fdcca8e541cf38fa0e3443e77112dbc7e63cce8
Petros-13 <petroskalkounos78@gmail.com>
2025-07-17 15:56:46 +0200

    2nd commit

---
+++ Main2.py
@@ -0,0 +1,11 @@
+print("enter your name")
+name=input("enter your name")
+print("Welcome",name,"to this class")
+x=5
+y=6
+sum=x+y
+print("The sum of x+y is",sum)
+x="5"
+y="6"
+sum=x+y
+print("The sum of x+y is",sum)
