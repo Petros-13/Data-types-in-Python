0593c3eba5e87ff43e61c37be606cdce57faba3e
Petros-13 <petroskalkounos78@gmail.com>
2025-07-17 15:40:10 +0200

    1st commit

---
+++ Main.py
@@ -0,0 +1,14 @@
+contry="Greece"
+weight=59.6
+age=13
+is_student=True
+print("I live in",contry)
+print("I weigh",weight,"Kg")
+print("I am",age,"old")
+print("Petros is a student.",is_student)
+
+print("Data type of",contry,"is",type(contry))
+print("Data type of",weight,"is",type(weight))
+print("Data type of",age,"is",type(age))
+print("Data type of",is_student,"is",type(is_student))
+
